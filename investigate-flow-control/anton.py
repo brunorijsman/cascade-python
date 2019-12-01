57a7b572c1befeb89db347042f4dba65f6da82eb
Bruno Rijsman <brunorijsman@gmail.com>
2019-12-01 15:00:34 -0600

    Get rid of flow-control investigation

--- investigate-flow-control/anton.py
+++
@@ -1,9 +0,0 @@
-import time
-import cqc.pythonLib as cqclib
-
-with cqclib.CQCConnection("Anton") as simulaqron:
-    while True:
-        qubit = cqclib.qubit(simulaqron)
-        simulaqron.sendQubit(qubit, "Bart")
-        print("[Anton] Sent qubit to Bart")
-        time.sleep(0.1)
--- investigate-flow-control/bart.py
+++
@@ -1,9 +0,0 @@
-import time
-import cqc.pythonLib as cqclib
-
-with cqclib.CQCConnection("Bart") as simulaqron:
-    while True:
-        qubit = simulaqron.recvQubit()
-        bit = qubit.measure()
-        print(f"[Bart] Received qubit {bit} from Anton")
-        time.sleep(0.2)

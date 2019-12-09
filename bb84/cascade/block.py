2a858e78ac2b4cfdd13151f028194cc006760604
Bruno Rijsman <brunorijsman@gmail.com>
2019-12-09 11:18:20 -0600

    blocks progress

--- bb84/cascade/block.py
+++ bb84/cascade/block.py
@@ -27,6 +27,38 @@ class Block:
         self._start_index = start_index
         self._end_index = end_index
 
+    @staticmethod
+    def create_blocks_covering_shuffle(shuffle, block_size):
+        """
+        Create a list of blocks of a given size that cover a given shuffled key.
+
+        Params:
+            shuffle (Shuffle): The shuffled key to be covered by the blocks.
+            block_size (int): The size of each block. Each block in the list, except for the last
+                one, will be exactly this size. The last block may be smaller.
+
+        Returns:
+            A list of blocks that cover the shuffled key.
+        """
+
+        # Validate arguments.
+        assert isinstance(block_size, int)
+        assert block_size > 0
+        assert isinstance(shuffle, Shuffle)
+
+        # Generate the blocks.
+        blocks = []
+        remaining_bits = shuffle.size
+        start_index = 0
+        while remaining_bits > 0:
+            actual_block_size = min(block_size, remaining_bits)
+            end_index = start_index + actual_block_size
+            block = Block(shuffle, start_index, end_index)
+            blocks.append(block)
+            start_index += actual_block_size
+            remaining_bits -= actual_block_size
+        return blocks
+
     def __repr__(self):
         """
         Get the unambiguous string representation of the block.
--- bb84/cascade/tests/test_block.py
+++ bb84/cascade/tests/test_block.py
@@ -37,3 +37,68 @@ def test_create_block():
     # Special case: empty block
     block = Block(shuffle, 2, 2)
     assert block.__repr__() == "Block:"
+
+def test_create_blocks_covering_shuffle():
+
+    key = Key.create_random_key(16, seed=234)
+    assert key.__repr__() == "Key: 1101101111011101"
+    shuffle = Shuffle(key, Shuffle.ALGORITHM_RANDOM, seed=345)
+    assert shuffle.__repr__() == ("Shuffle: 0->6=1 1->7=1 2->9=1 3->3=1 4->14=0 5->11=1 "
+                                  "6->8=1 7->5=0 8->2=0 9->15=1 10->10=0 11->4=1 12->1=1 "
+                                  "13->13=1 14->0=1 15->12=1")
+
+    # Multiple blocks, last block is partially filled
+    blocks = Block.create_blocks_covering_shuffle(shuffle, 5)
+    assert len(blocks) == 4
+    assert blocks[0].__repr__() == "Block: 0->6=1 1->7=1 2->9=1 3->3=1 4->14=0"
+    assert blocks[1].__repr__() == "Block: 5->11=1 6->8=1 7->5=0 8->2=0 9->15=1"
+    assert blocks[2].__repr__() == "Block: 10->10=0 11->4=1 12->1=1 13->13=1 14->0=1"
+    assert blocks[3].__repr__() == "Block: 15->12=1"
+
+    # Multiple blocks, last block is fully filled
+    blocks = Block.create_blocks_covering_shuffle(shuffle, 4)
+    assert len(blocks) == 4
+    assert blocks[0].__repr__() == "Block: 0->6=1 1->7=1 2->9=1 3->3=1"
+    assert blocks[1].__repr__() == "Block: 4->14=0 5->11=1 6->8=1 7->5=0"
+    assert blocks[2].__repr__() == "Block: 8->2=0 9->15=1 10->10=0 11->4=1"
+    assert blocks[3].__repr__() == "Block: 12->1=1 13->13=1 14->0=1 15->12=1"
+
+    # Single block, partially filled
+    key = Key.create_random_key(4, seed=234)
+    assert key.__repr__() == "Key: 1101"
+    shuffle = Shuffle(key, Shuffle.ALGORITHM_RANDOM, seed=345)
+    assert shuffle.__repr__() == "Shuffle: 0->2=0 1->1=1 2->0=1 3->3=1"
+    blocks = Block.create_blocks_covering_shuffle(shuffle, 5)
+    assert len(blocks) == 1
+    assert blocks[0].__repr__() == "Block: 0->2=0 1->1=1 2->0=1 3->3=1"
+
+    # Single block, fully filled
+    blocks = Block.create_blocks_covering_shuffle(shuffle, 4)
+    assert len(blocks) == 1
+    assert blocks[0].__repr__() == "Block: 0->2=0 1->1=1 2->0=1 3->3=1"
+
+    # No blocks (empty shuffle)
+    key = Key.create_random_key(0, seed=234)
+    assert key.__repr__() == "Key: "
+    shuffle = Shuffle(key, Shuffle.ALGORITHM_RANDOM, seed=345)
+    assert shuffle.__repr__() == "Shuffle:"
+    blocks = Block.create_blocks_covering_shuffle(shuffle, 5)
+    assert len(blocks) == 0
+
+def test_repr():
+    key = Key.create_random_key(4, seed=234)
+    assert key.__repr__() == "Key: 1101"
+    shuffle = Shuffle(key, Shuffle.ALGORITHM_RANDOM, seed=345)
+    assert shuffle.__repr__() == "Shuffle: 0->2=0 1->1=1 2->0=1 3->3=1"
+    blocks = Block.create_blocks_covering_shuffle(shuffle, 5)
+    assert len(blocks) == 1
+    assert blocks[0].__repr__() == "Block: 0->2=0 1->1=1 2->0=1 3->3=1"
+
+def test_str():
+    key = Key.create_random_key(4, seed=234)
+    assert key.__str__() == "1101"
+    shuffle = Shuffle(key, Shuffle.ALGORITHM_RANDOM, seed=345)
+    assert shuffle.__str__() == "0111"
+    blocks = Block.create_blocks_covering_shuffle(shuffle, 5)
+    assert len(blocks) == 1
+    assert blocks[0].__str__() == "0111"

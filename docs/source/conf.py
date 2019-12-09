bd502a254f48af44cbd41c22240ac44a87a19088
Bruno Rijsman <brunorijsman@gmail.com>
2019-12-09 15:14:42 -0600

    Work on RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,14 +15,12 @@ release = '0.0.1'
 extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.autodoc',
-    'sphinx.ext.apidoc',
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
 
 def is_venv():
     return (hasattr(sys, 'real_prefix') or
@@ -32,20 +30,22 @@ def run_apidoc(_):
     modules = ['bb84']
     for module in modules:
         cur_dir = os.path.abspath(os.path.dirname(__file__))
-        print(f"cur_dir = {cur_dir}")
-        output_path = os.path.join(cur_dir, '_modules')
-        print(f"output_path = {output_path}")
-        cmd_path = 'sphinx-apidoc'
-        exclude_pattern = f"`find {module} -name tests`"
+        module = os.path.join(cur_dir, '..', '..', module)
+        output_dir = os.path.join(cur_dir, '_modules')
+        apidoc = 'sphinx-apidoc'
         if is_venv():
-            cmd_path = os.path.abspath(os.path.join(sys.prefix, 'bin', 'sphinx-apidoc'))
-        print([cmd_path, '-f', '-e', '-o', output_path, module, exclude_pattern])
-        cmd = f"{cmd_path} -f -e -o {output_path} {module} {exclude_pattern}"
+            apidoc = os.path.abspath(os.path.join(sys.prefix, 'bin', 'sphinx-apidoc'))
+        exclude_pattern = f"`find {module} -name tests`"
+        cmd = f"{apidoc} -f -e -o {output_dir} {module} {exclude_pattern}"
+
+        print(f"cur_dir = {cur_dir}")
+        print(f"module = {module}")
+        print(f"output_dir = {output_dir}")
+        print(f"apidoc = {apidoc}")
+        print(f"exclude_pattern = {exclude_pattern}")
+        print(f"cmd = {cmd}")
+
         subprocess.check_call(cmd, shell=True)
-
-
-        # subprocess.check_call([cmd_path, '-f', '-e', '-o', output_path, module, exclude_pattern],
-        #                       shell=True)
 
 def setup(app):
     app.connect('builder-inited', run_apidoc)

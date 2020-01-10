11d519ef1d75409a27d2ad91b6bd9b2adf0b97c5
Bruno Rijsman <brunorijsman@gmail.com>
2020-01-09 19:35:37 -0600

    Major refactor run_experiments

--- study/run_experiments.py
+++ study/run_experiments.py
@@ -12,6 +12,10 @@ from study.data_point import DataPoint
 
 DEFAULT_RUNS = 10         # TODO: Make this a larger number
 
+TOTAL_NR_DATA_POINTS = None
+
+CURRENT_DATA_POINT_NR = None
+
 def parse_command_line_arguments():
     parser = argparse.ArgumentParser(description="Run Cascade experiments")
     parser.add_argument('experiments_file_name', metavar="experiments-file", type=str,
@@ -20,8 +24,6 @@ def parse_command_line_arguments():
     parser.add_argument('-r', '--runs', type=int, default=DEFAULT_RUNS,
                         help=(f"number of reconciliation runs per experiment "
                               f"(default: {DEFAULT_RUNS})"))
-    parser.add_argument('-e', '--experiment-name', type=str,
-                        help=f"name of experiment to run (default: run all experiments)")
     args = parser.parse_args()
     return args
 
@@ -30,63 +32,97 @@ def parse_experiments_file(file_name):
         experiments = json.load(json_file)
     return experiments
 
-def select_experiment(experiments, experiment_name):
+def experiments_to_series(experiments):
+    all_series = []
     for experiment in experiments:
-        if experiment['experiment_name'] == experiment_name:
-            return [experiment]
-    sys.exit(f"Experiment name {experiment_name} not found")
+        if experiment['independent_variable'] == 'error_rate':
+            experiment_series = experiment_to_error_rate_series(experiment)
+        elif experiment['independent_variable'] == 'key_size':
+            experiment_series = experiment_to_key_size_series(experiment)
+        else:
+            assert False
+        all_series += experiment_series
+    return all_series
 
-def data_points_in_multiple_experiments(experiments):
-    total_nr_data_points = 0
-    for experiment in experiments:
-        total_nr_data_points += data_points_in_one_experiment(experiment)
-    return total_nr_data_points
+def experiment_to_error_rate_series(experiment):
+    series = []
+    for algorithm in make_list(experiment['algorithm']):
+        for key_size in make_list(experiment['key_size']):
+            serie = dict(name=f"algorithm={algorithm};key_size={key_size};error_rate=vary",
+                         algorithms=[algorithm],
+                         key_sizes=[key_size],
+                         error_rates=make_list(experiment['error_rate']))
+            series.append(serie)
+    return series
 
-def data_points_in_one_experiment(experiment):
-    error_rates = make_list(experiment['error_rate'])
-    key_sizes = make_list(experiment['key_size'])
-    return len(error_rates) * len(key_sizes)
+def experiment_to_key_size_series(experiment):
+    series = []
+    for algorithm in make_list(experiment['algorithm']):
+        for error_rate in make_list(experiment['error_rate']):
+            serie = dict(name=f"algorithm={algorithm};key_size=vary;error_rate={error_rate}",
+                         algorithms=[algorithm],
+                         key_sizes=make_list(experiment['key_size'], do_round=True),
+                         error_rates=[error_rate])
+            series.append(serie)
+    return series
 
-def run_multiple_experiments(experiments, runs):
-    total_nr_data_points = data_points_in_multiple_experiments(experiments)
-    data_point_nr = 0
-    for experiment in experiments:
-        data_point_nr = run_experiment(experiment, runs, data_point_nr, total_nr_data_points)
-
-def run_experiment(experiment, runs, start_data_point_nr, total_nr_data_points):
-    algorithm = experiment['algorithm']
-    error_rates = make_list(experiment['error_rate'])
-    key_sizes = [round(v) for v in make_list(experiment['key_size'])]
-    experiment_name = experiment['experiment_name']
-    data_file_name = "data__" + experiment_name
-    data_point_nr = start_data_point_nr
-    with open(data_file_name, mode="w") as data_file:
-        for key_size in key_sizes:
-            for error_rate in error_rates:
-                percent = data_point_nr / total_nr_data_points * 100.0
-                print(f"percent={percent:.2f} "
-                      f"algorithm={algorithm} "
-                      f"key_size={key_size} "
-                      f"error_rate={error_rate:.4f}")
-                data_point_nr += 1
-                produce_data_point(data_file, runs, algorithm, key_size, "exact", error_rate)
-    return data_point_nr
-
-def make_list(value):
+def make_list(value, do_round=False):
+    if isinstance(value, (int, float, str)):
+        return [value]
+    if isinstance(value, list):
+        return value
     if isinstance(value, dict):
         start = value['start']
         end = value['end']
-        assert ('step_size' in value) != ('step_factor' in value)   # logical xor
+        assert ('step_size' in value) != ('step_factor' in value)
         step_size = value.get('step_size', 0.0)
         step_factor = value.get('step_factor', 1.0)
         lst = []
         current = start
         while current <= end:
-            lst.append(current)
+            if do_round:
+                lst.append(round(current))
+            else:
+                lst.append(current)
             current += step_size
             current *= step_factor
         return lst
-    return value
+    assert False
+    return None
+
+def compute_total_nr_data_points(series):
+    global TOTAL_NR_DATA_POINTS
+    TOTAL_NR_DATA_POINTS = 0
+    for serie in series:
+        TOTAL_NR_DATA_POINTS += (len(serie['algorithms']) * 
+                                 len(serie['key_sizes']) * 
+                                 len(serie['error_rates']))
+
+def run_series(series):
+    # TODO: Run in parallel
+    global CURRENT_DATA_POINT_NR
+    CURRENT_DATA_POINT_NR = 0
+    for serie in series:
+        run_serie(serie)
+
+def run_serie(serie):
+    data_file_name = "data__" + serie['name']
+    with open(data_file_name, mode="w") as data_file:
+        for algorithm in serie['algorithms']:
+            for key_size in serie['key_sizes']:
+                for error_rate in serie['error_rates']:
+                    # TODO: put number of runs in definition
+                    produce_data_point(data_file, 10, algorithm, key_size, "exact", error_rate)
+                    report_data_point_done(algorithm, key_size, error_rate)
+
+def report_data_point_done(algorithm, key_size, error_rate):
+    global CURRENT_DATA_POINT_NR, TOTAL_NR_DATA_POINTS
+    CURRENT_DATA_POINT_NR += 1
+    percent = CURRENT_DATA_POINT_NR / TOTAL_NR_DATA_POINTS * 100.0
+    print(f"percent={percent:.2f} "
+          f"algorithm={algorithm} "
+          f"key_size={key_size} "
+          f"error_rate={error_rate:.4f}")
 
 def produce_data_point(data_file, runs, algorithm, key_size, error_method, error_rate):
     data_point = DataPoint(algorithm, key_size, error_rate, get_code_version())
@@ -142,10 +178,9 @@ def to_json(obj):
 def main():
     args = parse_command_line_arguments()
     experiments = parse_experiments_file(args.experiments_file_name)
-    if args.experiment_name is not None:
-        experiments = select_experiment(experiments, args.experiment_name)
-    runs = args.runs
-    run_multiple_experiments(experiments, runs)
+    series = experiments_to_series(experiments)
+    compute_total_nr_data_points(series)
+    run_series(series)
 
 if __name__ == "__main__":
     main()
